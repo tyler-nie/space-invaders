e7fb8c2f3e3907b1f77caf04cf6927ab8c2bf12b
Tyler Nie <tylernie@live.ca>
2020-07-14 18:38:54 -0700

    created initial player icon

--- main.py
+++ main.py
@@ -26,12 +26,29 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cooldown_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
+
+
 def main():
     run = True
     FPS = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+
+    ship = Ship(300, 650)
+
     clock = pygame.time.Clock()
 
     # Easily draws and renders the screen
@@ -46,6 +63,8 @@ def main():
 
         WINDOW.blit(lives_label, (10, 10))
         WINDOW.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        ship.draw(WINDOW)
 
         # Refreshes the display
         pygame.display.update()
